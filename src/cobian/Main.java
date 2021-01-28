d13cb68da747efdd42e4d3e0279aa144b45fe9d1
Jamie Hurd <jaoshu@bitbucket.org>
2021-01-27 19:37:15 -0700

    Jamie's reply

--- src/cobian/Main.java
+++ src/cobian/Main.java
@@ -4,4 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Edgar");
     }
+
+    System.out.print("Test confirmed. I say hello again--from Jamie!");
 }
