bd18132fd55c7830bdd1bf9d565c7f9901f3a5a5
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2021-01-29 09:03:54 -0800

    I commented on my code.

--- src/cobian/Main.java
+++ src/cobian/Main.java
@@ -2,7 +2,8 @@ package cobian;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Edgar");
+        System.out.print("Hello, my name is Edgar");
+        System.out.print("I hope I'm doing this right!")
     }
 
     System.out.print("Test confirmed. I say hello again--from Jamie!");
