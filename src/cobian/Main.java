e006775c668e8834f70cc6e6b63b1fe72e87d4fd
cfowl <goose_7@msn.com>
2021-01-30 11:01:10 -0700

    said hello to Edgar from Chris

--- src/cobian/Main.java
+++ src/cobian/Main.java
@@ -3,8 +3,7 @@ package cobian;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, my name is Edgar");
-        
+        System.out.print("Test confirmed. I say hello again--from Jamie!");
+        System.out.println("\nHello Edgar, my name is Chris.");
     }
-
-    System.out.print("Test confirmed. I say hello again--from Jamie!");
 }
