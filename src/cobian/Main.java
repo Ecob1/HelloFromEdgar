c4bfacfca5db5065c18596c552d8b8a44c17dab1
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2021-01-29 21:36:14 -0800

    Update Main.java

--- src/cobian/Main.java
+++ src/cobian/Main.java
@@ -3,7 +3,7 @@ package cobian;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, my name is Edgar");
-        System.out.print("I hope I'm doing this right!")
+        
     }
 
     System.out.print("Test confirmed. I say hello again--from Jamie!");
